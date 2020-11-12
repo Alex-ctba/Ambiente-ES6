4f613e9d448eaf5fddd70be6ec941bea2e825d2d
Alexsandro <Alex_ctba@icloud.com>
2020-11-12 14:55:18 -0300

    Atualizado arquivos

--- bundle.js
+++ bundle.js
@@ -6,26 +6,61 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var mylist = /*#__PURE__*/function () {
-  function mylist() {
-    _classCallCheck(this, mylist);
+/*class mylist{
+    constructor(){
+        this.arrayLIst = []
+    }
 
-    this.arrayLIst = [];
+    displysList(){
+        this.arrayLIst.push('New Element')
+        console.log(this.arrayLIst)       
+    }
+}
+
+const list = new mylist()
+
+document.querySelector('button').onclick = function(){
+    list.displysList()
+
+
+//Utilizando herança
+
+class list{
+    constructor(){
+        this.dados = []
+    }
+
+    add(dados){
+        this.dados.push(dados)
+        console.log(this.dados)
+    }
+}
+
+class mylist extends list{
+    
+}
+
+const lista = new mylist()
+
+document.querySelector('button').onclick = function(){
+    lista.add('New element')
+}
+
+*/
+//Utilizando metodo static
+var sum = /*#__PURE__*/function () {
+  function sum() {
+    _classCallCheck(this, sum);
   }
 
-  _createClass(mylist, [{
-    key: "displysList",
-    value: function displysList() {
-      this.arrayLIst.push('New Element');
-      console.log(this.arrayLIst);
+  _createClass(sum, null, [{
+    key: "dados",
+    value: function dados(a, b) {
+      return a + b;
     }
   }]);
 
-  return mylist;
+  return sum;
 }();
 
-var list = new mylist();
-
-document.querySelector('button').onclick = function () {
-  list.displysList();
-};
+console.log(sum.dados(12, 5));
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-class mylist{
+/*class mylist{
     constructor(){
         this.arrayLIst = []
     }
@@ -13,4 +13,37 @@ const list = new mylist()
 
 document.querySelector('button').onclick = function(){
     list.displysList()
+
+
+//Utilizando herança
+
+class list{
+    constructor(){
+        this.dados = []
+    }
+
+    add(dados){
+        this.dados.push(dados)
+        console.log(this.dados)
+    }
 }
+
+class mylist extends list{
+    
+}
+
+const lista = new mylist()
+
+document.querySelector('button').onclick = function(){
+    lista.add('New element')
+}
+
+*/
+
+//Utilizando metodo static
+class sum{
+    static dados(a, b){
+        return a + b
+    }
+}
+console.log(sum.dados(12,5))
